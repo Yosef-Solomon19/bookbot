0b1ce229804dbe0352e7f397c9d521781d41e0b3
Yosef-Solomon19 <solomon.yosef.e@gmail.com>
2025-08-20 12:42:19 -0400

    Refactor - move count_words function to stats.py, then import function to main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# stats is the name of the file and count_words is the name of the function you want to import 
+from stats import count_words
+
 def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)    
@@ -7,13 +10,5 @@ def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
-
-# New function that accepts the text from the book as a string
-# And returns the number of words in the string.
-#.split()method
-#Print the number of words to the console ? 
-def count_words(text):
-    return len(text.split())
-
 
 main()
---
+++ stats.py
@@ -0,0 +1,6 @@
+# New function that accepts the text from the book as a string
+# And returns the number of words in the string.
+#.split()method
+#Print the number of words to the console ? 
+def count_words(text):
+    return len(text.split())
