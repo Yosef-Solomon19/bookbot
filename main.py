df1ade26a35d7cd82579066f43d138712a1cbb0b
Yosef-Solomon19 <solomon.yosef.e@gmail.com>
2025-08-20 12:11:10 -0400

    Add function to accept text from the book, & return the number of words

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+def main():
+    file_path = "books/frankenstein.txt"
+    text = get_book_text(file_path)    
+    print(f"{count_words(text)} words found in the document")
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
 
-def main():
-    file_path = "books/frankenstein.txt"
-    text = get_book_text(file_path)
-    print(text)
+# New function that accepts the text from the book as a string
+# And returns the number of words in the string.
+#.split()method
+#Print the number of words to the console ? 
+def count_words(text):
+    return len(text.split())
+
 
 main()
