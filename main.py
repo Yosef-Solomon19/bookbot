e009c69988c5b3e3d5496842ca59c0b55281180e
Yosef-Solomon19 <solomon.yosef.e@gmail.com>
2025-09-01 13:30:55 -0400

    add if/else statemt to run the functions if sys.argv has two entries, replace arguements taking file path with sys.argv first argument.

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ from stats import get_num_characters
 from stats import sort_dictionary
 from stats import print_report
 
-def main():
-    file_path = "books/frankenstein.txt"
-    text = get_book_text(file_path)        
-    characters_result = get_num_characters(text)
-    sort_list =sort_dictionary(characters_result)
-    num_words=count_words(text)
-    book_report = print_report(file_path, num_words, sort_list )
-    return book_report
+if len(sys.argv) == 2:
 
-def get_book_text(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-        return file_contents
+    def main():
+        #file_path = "books/frankenstein.txt"
+        text = get_book_text(sys.argv[1])        
+        characters_result = get_num_characters(text)
+        sort_list =sort_dictionary(characters_result)
+        num_words=count_words(text)
+        book_report = print_report(sys.argv[1], num_words, sort_list )
+        return book_report
 
+    def get_book_text(file_path):
+        with open(file_path) as f:
+            file_contents = f.read()
+            return file_contents
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 main()
