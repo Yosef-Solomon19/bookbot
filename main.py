e0b29e77188c118cbc7e39cede9c0219038c4806
Yosef-Solomon19 <solomon.yosef.e@gmail.com>
2025-09-01 13:17:52 -0400

    import sys module

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# import sys module 
+import sys 
 # stats is the name of the file and count_words is the name of the function you want to import 
 from stats import count_words
 from stats import get_num_characters 
